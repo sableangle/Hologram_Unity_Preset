65cab921ca3187d43ea08ae18b563a8c9f7683a6
MikiSahika <sableangle@gmail.com>
2016-09-21 18:44:23 +0800

    First on git

---
+++ Assets/TextScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextScript : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate (0,50 * Time.deltaTime,0);
+	}
+}
